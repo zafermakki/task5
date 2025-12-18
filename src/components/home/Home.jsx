2a1776a76129e7982b7fe8cc0c3008d43206aa7e
unknown <zafermakki@gmail.com>
2025-12-18 20:42:43 +0200

    edit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Box } from "@mui/material";
 import Navbar from "../../components/home/navBar/Navbar";
 import HeroSection from "../../components/home/heroSection/HeroSection";
-import Features from "../../components/home/features/Features";
 
 const Home = ({setmyMOde}) => {
   return (
     <Box>
       <Navbar setmyMOde={setmyMOde} />
       <HeroSection />
-      <Features />
     </Box>
   );
 };
